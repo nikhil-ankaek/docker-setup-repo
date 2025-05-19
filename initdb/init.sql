a8ba951ec6a84fe116fda2e15db3c7de471937a7
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2025-05-19 19:52:55 +0545

    first commit

---
+++ initdb/init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE aok_connect_auth;
+CREATE DATABASE aok_connect_individual;
+CREATE DATABASE aok_connect_business;
+CREATE DATABASE aok_connect_connection;
+CREATE DATABASE aok_connect_notification;
+CREATE DATABASE aok_connect_chat;
+CREATE DATABASE aok_connect_support;
+CREATE DATABASE aok_connect_address;
